535a616bd8c8d1f981face66cbd53c2717ed4ef5
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-07 19:12:34 -0500

    feat: get req for next videos working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const videosRoute = require('./routes/videos')
 
 app.use(express.json())
 app.use(express.static('public'))
+app.use('/videos', videosRoute)
 
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
--- routes/videos.js
+++ routes/videos.js
@@ -3,3 +3,21 @@ const router = express.Router();
 const fs = require('fs');
 var uniqid = require('uniqid')
 const videosFile = fs.readFileSync('./data/videos.json')
+const videos = JSON.parse(videosFile)
+const videoList = videos.map(video => {
+    let simpleVideo = {
+        id: video.id,
+        title: video.title,
+        channel: video.channel,
+        image: video.image
+    }
+    return simpleVideo
+})
+
+router.route('/')
+.get((req, res) => {
+    console.log(videoList)
+    res.send("gotcha")
+})
+
+module.exports = router
