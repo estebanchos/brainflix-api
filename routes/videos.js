9ede6a822d138782ad7b310a97efb6ac9a705977
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-07 23:22:52 -0500

    feat: post comment implemented

--- routes/videos.js
+++ routes/videos.js
@@ -15,15 +15,17 @@ const videoList = videos.map(video => {
     return simpleVideo
 })
 
+const findVideo = (id) => videos.find(video => video.id === id)
+
 router.route('/')
-    .get((req, res) => {
-        res.status(201).send(videoList)
+    .get((_req, res) => {
+        res.status(200).send(videoList)
     })
 
 router.route('/:videoId')
     .get((req, res) => {
-        let found = videos.find(video => video.id === req.params.videoId)
-        if(!found) {
+        let found = findVideo(req.params.videoId)
+        if (!found) {
             res.status(404).send("Incorrect Video ID")
         } else {
             const fullDetails = {
@@ -39,9 +41,30 @@ router.route('/:videoId')
                 timeStamp: found.details.timestamp,
                 comments: found.details.comments
             }
-            res.status(201).send(fullDetails)
+            res.status(200).send(fullDetails)
         }
     })
 
+router.route('/:videoId/comments')
+    .post((req, res) => {
+        let found = findVideo(req.params.videoId)
+        if (!found) {
+            res.status(404).send("Incorrect Video ID")
+        } else {
+            const newComment = {
+                id: uniqid(),
+                name: req.body.name,
+                comment: req.body.comment,
+                likes: 0,
+                timestamp: Date.now()
+            }
+            res.status(201).send(newComment)
+            let updatedComments = [...found.details.comments, newComment]
+            let foundIndex = videos.findIndex(video => video.id === req.params.videoId)
+            let newVideos = [...videos]
+            newVideos[foundIndex].details.comments = updatedComments
+            fs.writeFileSync('./data/videos.json', JSON.stringify(newVideos))
+        }
+    })
 
 module.exports = router
