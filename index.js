abac83c39da9364ab8c2d598f50848696c5a7732
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-08 19:54:10 -0500

    update: cors implemented

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 const { PORT: port } = process.env
+const cors = require('cors')
 const videosRoute = require('./routes/videos')
 
 app.use(express.json())
+app.use(cors())
 app.use(express.static('public'))
 app.use('/videos', videosRoute)
 
